d0ac590b7aacbd3f6b78d4d1d28e9045a6d5c26d
DawnBits <isadorabrunos1@gmail.com>
2021-08-28 16:42:44 -0300

    yes

--- routes.js
+++ routes.js
@@ -1,12 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const faker = require("faker");
 
 let db = require(",/db");
 
 router.get('/',(req,res)=> {
     res.render('pages/home');
-    res.send('Página de teste');
+});
+
+router.get('/about',(req,res)=>{
+
+    res.render('pages/home');
+
+});
+
+router.get('/cadastro',(req,res)=>{
+
+    res.render('pages/cadastro',{users:users});
+
+});
+
+router.post('/cadastro/remove',(req,res)=>{
+ 
+    let name = req.body.name;
+
+    if(users.length==0){
+        console.log("Erro: Não há elemento a ser removido!");
+        return res.status(500).json({
+            status:'error',
+            error:`Removed element: ${name}`
+        });
+
+    } else {
+        for(let cont=0;cont<users.length;cont++){
+            if(users[cont].name==name){
+                users.splice(cont,1);
+                console.log("Elemento Removido: ",name);
+                return res.status(200).json({
+                    status:'sucess',
+                    data:users
+                });
+
+            } else if(cont==users.length-1){
+                console.log("Erro ao remover elemento: ",name);
+                return res.status(400).json({
+                    status:'error',
+                    error:`Didn't Remove element: ${name}`
+                });
+            }
+        }
+    }
 });
 
 module.exports = router;
